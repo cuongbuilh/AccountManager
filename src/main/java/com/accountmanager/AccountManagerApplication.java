d9f603e347936bd06adcde3df6f861e2495885ad
CuongBui <cuongbuilh@gmail.com>
2021-09-03 01:18:54 +0700

    add maven hello page

--- src/main/java/com/accountmanager/AccountManagerApplication.java
+++ src/main/java/com/accountmanager/AccountManagerApplication.java
@@ -9,6 +9,7 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScans;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,9 +21,15 @@ import org.springframework.web.bind.annotation.RestController;
 @ComponentScan(basePackageClasses = Position.class)
 @EntityScan("entity")
 @EnableJpaRepositories("repository")
+@Controller
 public class AccountManagerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AccountManagerApplication.class, args);
+    }
+
+    @RequestMapping("/")
+    String home(){
+        return "hello api";
     }
 }
